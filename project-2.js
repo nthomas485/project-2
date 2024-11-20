54396b4f7f0f818d76104889b4b0996581cd16c8
nthomas485 <nat5363@psu.edu>
2024-11-20 12:56:09 -0500

    imports and inputs

--- project-2.js
+++ project-2.js
@@ -5,6 +5,35 @@
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+import "@haxtheweb/rpg-character/rpg-character.js";
+import { WiredButton, WiredInput } from "wired-elements"
+
+export const hatArray = [
+  "bunny",
+  "coffee",
+  "construction",
+  "cowboy",
+  "education",
+  "knight",
+  "ninja",
+  "party",
+  "pirate",
+  "watermelon",
+];
+
+/* export const characterAttributes = [
+  accessories = '9',
+  base = '1',
+  leg = ["", "R", "L"],
+  face = '5',
+  faceItem = '9',
+  hair = '9',
+  pants = '9',
+  shirt = '9',
+  skin = '9',
+  hatColor = '9',
+]; 
+*/
 
 /**
  * `project-2`
@@ -21,18 +50,28 @@ export class Project2 extends DDDSuper(I18NMixin(LitElement)) {
   constructor() {
     super();
     this.title = "";
-    this.t = this.t || {};
-    this.t = {
-      ...this.t,
-      title: "Title",
-    };
-    this.registerLocalization({
-      context: this,
-      localesPath:
-        new URL("./locales/project-2.ar.json", import.meta.url).href +
-        "/../",
-      locales: ["ar", "es", "hi", "zh"],
-    });
+    this.accentColor = '';
+    this.dark = '';
+    this.accessories = '';
+    this.height = '';
+    this.width = '';
+    this.base = '';
+    this.face = '';
+    this.faceItem = '';
+    this.hair = '';
+    this.pants = '';
+    this.shirt = '';
+    this.skin = '';
+    this.hatColor = '';
+    this.hat = '';
+    this.walking = false;
+    this.leg = [];
+    this.seed = null;
+    this.speed = '';
+    this.circle = false;
+    this.fire = false;
+    this.demo = false;
+    this.reduceMotion = true;
   }
 
   // Lit reactive properties
@@ -40,6 +79,27 @@ export class Project2 extends DDDSuper(I18NMixin(LitElement)) {
     return {
       ...super.properties,
       title: { type: String },
+      accentColor: { type: String },
+      dark: { type: String },
+      accessories: { type: String},
+      height: { type: String },
+      width: {type: String },
+      base: { type: String },
+      face: { type: String },
+      faceItem: {type: String },
+      hair: { type: String },
+      pants: { type: String },
+      shirt: {type: String },
+      skin: { type: String },
+      hatColor: { type: String },
+      hat: { type: String },
+      walking: { type: Boolean, Reflect: true},
+      leg: { type: Array },
+      speed: { type: String },
+      circle: { type: Boolean, Reflect: true },
+      fire: { type: Boolean, Reflect: true },
+      demo: { type: Boolean, Reflect: true },
+      reduceMotion: { type: Boolean, Reflect: true },
     };
   }
 
@@ -68,6 +128,8 @@ export class Project2 extends DDDSuper(I18NMixin(LitElement)) {
     return html`
 <div class="wrapper">
   <h3><span>${this.t.title}:</span> ${this.title}</h3>
+  <rpg-character ></rpg-character>
+  <wired-button> Click </wired-button>
   <slot></slot>
 </div>`;
   }
